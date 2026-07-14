345b9f4d79572e9734daf55d2c3e446d162a9964
daniel-000com <danielorrala5552@gmail.com>
2026-07-13 22:19:06 -0500

    Add utilitario.js and the functions eliminarIzquierda & pintarArregloIzquierda

--- arreglos.js
+++ arreglos.js
@@ -1,10 +1,11 @@
 let numEada =[];
+let numeroCaja = []
+let edadesDerecha = []
 
 function agregarEdad(){
-    let numeroCaja
     numeroCaja = recuperarInt("edad");
     numEada.push(numeroCaja);
-    pintarArregloIzquierda(numeroCaja);
+    pintarArregloIzquierda(numEada);
    
   }
 
@@ -16,23 +17,28 @@ function pintarArregloIzquierda(numeroCaja){
     for(let i = 0; i < numEada.length; i ++){
      let addEdad = numEada[i];
        agregarNumero += "<tr><td>"+(addEdad)+"</td>";
-       agregarNumero += "<td><button class='btn-eliminar'>Eliminar</button></td>";
+       agregarNumero += "<td><button onclick='eliminarIzquierda("+i+")' class='btn-eliminar'>Eliminar</button></td>";
        agregarNumero += "<td><button class='btn-mover'>➜</button></tr></tr>"    
    } 
    cmpTabla.innerHTML= agregarNumero;
 }
 
-function recuperaraTexto(idComponente){
-    let componente;
-    let valorIngresado;
-    componente=document.getElementById(idComponente);
-    valorIngresado=componente.value;
-    return valorIngresado;
-    }
-
-function recuperarInt(idComponente){
-        let valorCaja=recuperaraTexto(idComponente);
-        let valorEntero=parseInt(valorCaja);
-        return valorEntero;
+function eliminarIzquierda(indice){
+     numEada.splice(indice, 1)
+     pintarArregloIzquierda(numEada)
+     
 }
+function pintarArregloIzquierda(numeroCaja){
+    let cmpTabla;
+    let agregarNumero ="";
+    cmpTabla = document.getElementById("tablaDerecha");
+    for(let i = 0; i < numEada.length; i ++){
+     let addEdad = numEada[i];
+       agregarNumero += "<td><button class='btn-mover'>⬅</button></tr></tr>"
+       agregarNumero += "<tr><td>"+(addEdad)+"</td>";
+       agregarNumero += "<td><button onclick='eliminarIzquierda("+i+")' class='btn-eliminar'>Eliminar</button></td>";    
+   } 
+   cmpTabla.innerHTML= agregarNumero;
+}
+
 
